28834eb854c61fdd2bb6c3dc4742738286d1322d
Catpink369 <camilamahecha369@gmail.com>
2025-08-08 19:06:33 -0600

    agrego las funciones de division y multiplicacion al codigo con suma y resta

--- Funciones.py
+++ Funciones.py
@@ -1,9 +1,9 @@
 #En este archivo se crearan las funciones de la calculadora#
 def suma(a, b):
-return a + b
+    return a + b
 
 def resta(a, b):
-return a - b
+    return a - b
 
 def multiplicacion (a,b):
     return a * b
--- Menu.py
+++ Menu.py
@@ -1,26 +1,38 @@
-#Operaciones de la calculadora#
+from funciones import suma, resta, multiplicacion, division
+
 def main():
-while True:
-print("Calculadora")
-print("1. Suma")
-print("2. Resta")
-print("3. Salir")
+    while True:
+        print("Calculadora")
+        print("1. Suma")
+        print("2. Resta")
+        print("3. Multiplicación")
+        print("4. División")
+        print("5. Salir")
 
-opcion = input("Elige una opción: ")
-match opcion:
-case "1":
-a = float(input("Primer número: "))
-b = float(input("Segundo número: "))
-print(f"Resultado: {suma(a, b)}")
-case "2":
-a = float(input("Primer número: "))
-b = float(input("Segundo número: "))
-print(f"Resultado: {resta(a, b)}")
-case "3":
-print("Saliendo...")
-break
-case _:
-print("Opción no válida")
+        opcion = input("Elige una opción: ")
 
-if __name__ == "__main__":
-main()
+        match opcion: 
+            case "1":
+                a = float(input("Primer número: "))
+                b = float(input("Segundo número: "))
+                print(f"Resultado: {suma(a, b)}")  
+            case "2":
+                a = float(input("Primer número: "))
+                b = float(input("Segundo número: "))
+                print(f"Resultado: {resta(a, b)}") 
+            case "3":
+                a = float(input("Primer número: "))
+                b = float(input("Segundo número: "))
+                print(f"Resultado: {multiplicacion(a, b)}")
+            case "4":
+                a = float(input("Primer número: "))
+                b = float(input("Segundo número: "))
+                print(f"Resultado: {division(a, b)}")
+            case "5":
+                print("Saliendo...")
+                break
+            case _:
+                print("Opción no válida")
+
+if _name_ == "_main_":
+    main()
