d6eb442cb39e486b88c473578ab5d36ac9ec058f
Catpink369 <camilamahecha369@gmail.com>
2025-08-08 18:40:42 -0600

    agrego funciones para multiplicacion y division

--- Funciones.py
+++ Funciones.py
@@ -1,1 +1,5 @@
-#En este archivo se crearan las funciones de la calculadora#
+#En este archivo se crearan las funciones de la calculadora
+def multiplicacion (a,b):
+    return a * b
+def division (a,b):
+    return a / b
